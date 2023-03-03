93eb814b003a2f4cb3dbb183bd4774c44cb8b1b5
jmmcDesign <35576283+jmmolins87@users.noreply.github.com>
2023-03-03 15:44:30 +0100

    wip

---
+++ src/app/components/short-url/short-url.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-short-url',
+  templateUrl: './short-url.component.html',
+  styleUrls: ['./short-url.component.scss']
+})
+export class ShortUrlComponent implements OnInit {
+
+  public nameUrl: string = ''
+  public urlProcessed: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
